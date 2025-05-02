8342592478e463373a34456b80189cfcc8228e94
slurpy-films <mattisiversenholte@gmail.com>
2025-05-02 13:10:29 +0200

    Added recursive class inheritance and super()

--- include/runtime/eval/newexpr.hpp
+++ include/runtime/eval/newexpr.hpp
@@ -8,6 +8,8 @@
 #include "fndeclaration.hpp"
 #include "assignment.hpp"
 
+void inheritClass(ClassVal* cls, Env* env, ObjectVal* thisObj, std::vector<RuntimeVal*> args);
+
 RuntimeVal* evalNewExpr(NewExprType* newexpr, Env* env) {
     RuntimeVal* rawcls = eval(newexpr->constructor, env);
 
@@ -24,59 +26,10 @@ RuntimeVal* evalNewExpr(NewExprType* newexpr, Env* env) {
     }
     
     Env* scope = new Env(env);
-    scope->declareVar("this", new ObjectVal());
+    ObjectVal* thisObj = new ObjectVal();
+    scope->declareVar("this", thisObj);
 
-
-    
-    if (cls->doesExtend) {
-        RuntimeVal* extendsVal = eval(cls->extends, cls->parentEnv);
-        if (extendsVal->type != ValueType::Class) {
-            std::cerr << "Superclass must be a class.\n";
-            exit(1);
-        }
-    
-        ClassVal* superClass = static_cast<ClassVal*>(extendsVal);
-    
-        ObjectVal* childThis = static_cast<ObjectVal*>(scope->lookupVar("this"));
-        Env* tempScope = new Env(cls->parentEnv);
-        tempScope->declareVar("this", childThis);
-        
-    
-        for (Stmt* stmt : superClass->body) {
-            if (stmt->kind == NodeType::FunctionDeclaration) {
-                FunctionValue* fnval = static_cast<FunctionValue*>(evalFunctionDeclaration(static_cast<FunctionDeclarationType*>(stmt), tempScope, true));
-                childThis->properties[fnval->name] = fnval;
-            } else if (stmt->kind == NodeType::AssignmentExpr) {
-                AssignmentExprType* assign = static_cast<AssignmentExprType*>(stmt);
-                if (assign->op != "=") {
-                    std::cerr << "Only = assignment is allowed in class bodies";
-                    exit(1);
-                }
-
-                Env* assignEnv = new Env();
-
-                assignEnv->declareVar(static_cast<IdentifierType*>(assign->assigne)->symbol, new UndefinedVal());
-
-                evalAssignment(assign, assignEnv);
-
-                childThis->properties[static_cast<IdentifierType*>(assign->assigne)->symbol] = assignEnv->variables[static_cast<IdentifierType*>(assign->assigne)->symbol];
-            }
-        }
-    
-        if (childThis->properties.find("constructor") != childThis->properties.end()) {
-            scope->declareVar("super", childThis->properties["constructor"]);
-        }
-
-        ObjectVal* subThis = static_cast<ObjectVal*>(scope->lookupVar("this"));
-
-        for (const auto& [key, val] : static_cast<ObjectVal*>(tempScope->lookupVar("this"))->properties) {
-            if (subThis->properties.find(key) == subThis->properties.end()) {
-                subThis->properties[key] = val;
-            }
-        }
-
-        scope->assignVar("this", subThis);
-    }
+    inheritClass(cls, scope, thisObj, args);
 
     for (Stmt* stmt : cls->body) {
         switch (stmt->kind) {
@@ -127,4 +80,48 @@ RuntimeVal* evalNewExpr(NewExprType* newexpr, Env* env) {
     }
 
     return scope->lookupVar("this");
+}
+
+void inheritClass(ClassVal* cls, Env* env, ObjectVal* thisObj, std::vector<RuntimeVal*> args) {
+    if (!cls->doesExtend) return;
+
+    RuntimeVal* extendsVal = eval(cls->extends, cls->parentEnv);
+    if (extendsVal->type != ValueType::Class) {
+        std::cerr << "Superclass must be a class.\n";
+        exit(1);
+    }
+    Env* superScope = new Env(cls->parentEnv);
+    ClassVal* superClass = static_cast<ClassVal*>(extendsVal);
+    superScope->declareVar("this", thisObj);
+
+    inheritClass(superClass, superScope, thisObj, args);
+
+    RuntimeVal* cons;
+    bool hasCons = false;
+
+    for (Stmt* stmt : superClass->body) {
+        if (stmt->kind == NodeType::FunctionDeclaration) {
+            FunctionValue* fnval = static_cast<FunctionValue*>(evalFunctionDeclaration(static_cast<FunctionDeclarationType*>(stmt), superScope, true));
+            if (fnval->name == "constructor") {
+                cons = fnval;
+                hasCons = true;
+            } else thisObj->properties[fnval->name] = fnval;
+        } else if (stmt->kind == NodeType::AssignmentExpr) {
+            AssignmentExprType* assign = static_cast<AssignmentExprType*>(stmt);
+            if (assign->op != "=") {
+                std::cerr << "Only = assignment is allowed in class bodies";
+                exit(1);
+            }
+
+            Env* assignEnv = new Env();
+            assignEnv->declareVar(static_cast<IdentifierType*>(assign->assigne)->symbol, new UndefinedVal());
+            evalAssignment(assign, assignEnv);
+            thisObj->properties[static_cast<IdentifierType*>(assign->assigne)->symbol] = assignEnv->variables[static_cast<IdentifierType*>(assign->assigne)->symbol];
+        }
+    }
+
+    if (hasCons) {
+        env->declareVar("super", cons);
+    }
+
 }
