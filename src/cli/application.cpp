1457d254083f86cd3b0268f4276d1ccb90b5b072
slurpy-films <=>
2025-08-13 20:47:01 +0200

    Fixed extern initialization in g_currentCwd

--- src/cli/application.cpp
+++ src/cli/application.cpp
@@ -2,7 +2,7 @@
 
 using namespace Probescript;
 
-extern fs::path g_currentCwd = std::filesystem::current_path();
+extern fs::path g_currentCwd;
 
 extern std::unordered_map<std::string, VM::ValuePtr> g_valueGlobals;
 
